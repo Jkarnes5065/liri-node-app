1e2aff76d9259734773d8a84c7b8ff52ef674689
Julie Karnes <julie.anne.karnes@gmail.com>
2017-07-19 18:30:15 -0500

    updated liri.js

--- liri.js
+++ liri.js
@@ -44,7 +44,7 @@ var getMeSpotify = function(songName){
 
 	    for(var i=0; i<songs.length; i++){
 	    	console.log(i);
-	    	console.log('artist(s): ' + songs[i].artists.map(getArtistsNames));
+	    	console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
 	    	console.log('song name: ' + songs[i].name);
 	    	console.log('preview song: ' + songs[i].preview_url);
 	    	console.log('album: ' + songs[i].album.name);
